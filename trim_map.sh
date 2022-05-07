67aff0318f1c95ebb27ab60cea28178174f1f213
brross60 <64081484+brross60@users.noreply.github.com>
2022-05-07 18:33:11 -0400

    Update trim_map.sh

--- trim_map.sh
+++ trim_map.sh
@@ -13,17 +13,6 @@ do
   cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -m 22 -o "$i_basename"_22bp.trim.fastq "$i_basename".fastq > "$i_basename"_22bp.cutadapt_log.txt
 done
 
-#################################
-
-trim_filenames=($(ls *.trim.fastq))
-
-for i in "${trim_filenames[@]}"
-do
-  i_basename=$(basename $i .trim.fastq)
-  echo "mapping $i_basename with bowtie2 ..."
-  bowtie2 --end-to-end -p 20 -x decoyGLBR -q -U "$i_basename".trim.fastq -S decoyGLBR_"$i_basename".sam --un non-decoyGLBR_"$i_basename".fastq 2>> decoyGLBR_"$i_basename".bowtie_output.txt
-done
-
 ###############################################
 
 ### REAGULAR MAPPING WITH Bowtie2 ###
@@ -33,32 +22,37 @@ for i in "${trim_filenames[@]}"
 do
   i_basename=$(basename $i .trim.fastq)
   echo "mapping $i_basename with bowtie2 ..."
-  bowtie2 --end-to-end --very-sensitive -p 8 -x /nv/hp10/bross60/data/ref_genomes/mmas.JM15300 -q -U "$i_basename".trim.fastq -S MMAS_NCBI_"$i_basename".sam 2>> MMAS_NCBI_"$i_basename".bowtie_output.txt
+  bowtie2 --end-to-end --very-sensitive -p 8 -x /storage/home/hcoda1/9/bross60/p-mwhiteley3-0/rich_project_bio-whiteley/ref_genomes/MAB/MABATCC19977 -q -U "$i_basename".trim.fastq -S MMAS_NCBI_"$i_basename".sam 2>> MMAS_NCBI_"$i_basename".bowtie_output.txt
 done
 
 ##########################################################
 
 ### REMOVING HUMAN (HUMANGRCh38)READS WITH Bowtie2 ###version 2.4.2
-trim_filenames=($(ls *.trim.fastq))
+#bowtie2-build -f GRCh38_latest_genomic.fna HUMANGRCh38
+
+trim_filenames=($(ls *trim.fastq))
 
 for i in "${trim_filenames[@]}"
 do
-  i_basename=$(basename $i .trim.fastq)
-  echo "mapping $i_basename with bowtie2 ..."
-  bowtie2 --end-to-end -p 20 -x /nv/hp10/bross60/data/ref_genomes/HUMAN/HUMANGRCh38 -q -U "$i_basename".trim.fastq -S toHuman_"$i_basename".sam --un noHuman_"$i_basename".fastq 2>> Human_"$i_basename".bowtie_output.txt
+  i_basename=$(basename $i trim.fastq)
+  echo "mapping to human $i_basename ..."
+  bowtie2 --end-to-end -p 20 -x /storage/home/hcoda1/9/bross60/p-mwhiteley3-0/rich_project_bio-whiteley/ref_genomes/HUMAN/HUMANGRCh38 -q -U "$i_basename".fastq -S toHuman_"$i_basename".sam --un noHuman_"$i_basename".fastq 2>> Human_"$i_basename".bowtie_output.txt
 done
 
 ### REMOVEING DECOY (decoyGLBR) READS WITH Bowtie2 ### version 2.4.2
-bowtie2-build -f RedonePangenome_Apr12_2021_Jan16_2022.fasta decoyGLBR
+#bowtie2-build -f RedonePangenome_Apr12_2021_Jan16_2022.fasta decoyGLBR
 
-trim_filenames=($(ls *.trim.fastq))
+filenames=($(ls *.fastq))
 
-for i in "${trim_filenames[@]}"
+for i in "${filenames[@]}"
 do
-  i_basename=$(basename $i .trim.fastq)
+  i_basename=$(basename $i .fastq)
   echo "mapping $i_basename with bowtie2 ..."
-  bowtie2 --end-to-end --very-sensitive -p 20 -x /nv/hp10/bross60/data/ref_genomes/DECOY/decoyGLBR -q -U noHuman_"$i_basename".fastq -S noHuman_toDecoyGLBR_"$i_basename".sam --un noHuman_noDecoyGLBR_"$i_basename".fastq 2>> decoyGLBR_"$i_basename".bowtie_output.txt
+  bowtie2 --end-to-end --very-sensitive -p 20 -x decoyGLBR -q -U "$i_basename".fastq -S noHuman_toDecoyGLBR_"$i_basename".sam --un noHuman_noDecoyGLBR_"$i_basename".fastq 2>> decoyGLBR_"$i_basename".bowtie_output.txt
 done
+
+mkdir todecoy
+mv noHuman_toDecoyGLBR*.sam todecoy
 
 ### MAPPING TO MAB (MABATCC19977) WITH Bowtie2 ### version 2.4.2
 #bowtie2-build GCF_000195955.2_ASM19595v2_genomic.fna H37Rv
