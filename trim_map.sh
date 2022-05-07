4db82929c432ccfe54d554e353f77b722f1b2353
brross60 <64081484+brross60@users.noreply.github.com>
2022-05-07 11:34:02 -0400

    Update trim_map.sh

--- trim_map.sh
+++ trim_map.sh
@@ -3,6 +3,16 @@ conda activate mapping
 cd xxx
 
 #################################
+
+trim_filenames=($(ls *.trim.fastq))
+
+for i in "${trim_filenames[@]}"
+do
+  i_basename=$(basename $i .trim.fastq)
+  echo "mapping $i_basename with bowtie2 ..."
+  bowtie2 --end-to-end -p 20 -x decoyGLBR -q -U "$i_basename".trim.fastq -S decoyGLBR_"$i_basename".sam --un non-decoyGLBR_"$i_basename".fastq 2>> decoyGLBR_"$i_basename".bowtie_output.txt
+done
+
 ###############################################
 
 ### REAGULAR MAPPING WITH Bowtie2 ###
@@ -15,11 +25,6 @@ do
   bowtie2 --end-to-end -p 8 -x /nv/hp10/bross60/data/ref_genomes/mmas.JM15300 -q -U "$i_basename".trim.fastq -S MMAS_NCBI_"$i_basename".sam 2>> MMAS_NCBI_"$i_basename".bowtie_output.txt
 done
 
-### COUNTING FEATURES ###
-./featureCounts -a /nv/hp10/bross60/data/ref_genomes/MMAS_GCF_000497265.gff3 -g Parent -t CDS -s 1 -o featureCounts_MMAS_NCBI_EmoryHSP_18bp.txt MMAS_NCBI*.sam
-Done
-
-
 ##########################################################
 
 ### REMOVING HUMAN (HUMANGRCh38)READS WITH Bowtie2 ###version 2.4.2
@@ -31,7 +36,6 @@ do
   echo "mapping $i_basename with bowtie2 ..."
   bowtie2 --end-to-end -p 8 -x /nv/hp10/bross60/data/ref_genomes/HUMAN/HUMANGRCh38 -q -U "$i_basename".trim.fastq -S toHuman_"$i_basename".sam --un noHuman_"$i_basename".fastq 2>> Human_"$i_basename".bowtie_output.txt
 done
-
 
 ### REMOVEING DECOY (decoyGLBR) READS WITH Bowtie2 ### version 2.4.2
 bowtie2-build -f RedonePangenome_Apr12_2021_Jan16_2022.fasta decoyGLBR
@@ -57,7 +61,12 @@ do
   bowtie2 --end-to-end -p 20 -x /nv/hp10/bross60/data/ref_genomes/MAB/MABATCC19977 -q -U noHuman_noDecoyGLBR_"$i_basename".fastq -S MAB_noHuman_noDecoy"$i_basename".sam 2>> MAB_noHuman_noDecoy_"$i_basename".bowtie_output.txt
 done
 
+##############################################################################
+
+#OTHER REFS
 #/nv/hp10/bross60/data/ref_genomes/TB/H37Rv
+
+#################################################################################################################
 
 ### COUNTING FEATURES ###
 ./featureCounts -a /nv/hp10/bross60/data/ref_genomes/MAB/GCF_000069185.1_ASM6918v1_genomic.gff -g gene_id -t gene -O -s 0 -o featureCounts_[insertname]_22bp.txt *.sam
