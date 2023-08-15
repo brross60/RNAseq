16bad8bd47d0fec526efb8c9e4ff27d23240cbad
brross60 <64081484+brross60@users.noreply.github.com>
2023-08-15 11:43:36 -0400

    conda_enviorment.sh

--- conda_enviorment.sh
+++ conda_enviorment.sh
@@ -4,3 +4,6 @@ conda install cutadapt
 conda install bowtie2
 
 ## check python requiremnt for bowtie 2 and install paython3 if needed
+##in use
+##Bowtie 2 version 2.4.2 by Ben Langmead (langmea@cs.jhu.edu, www.cs.jhu.edu/~langmea)
+##This is cutadapt 3.3 with Python 3.9.7
